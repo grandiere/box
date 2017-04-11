311794c584587da4426557659d594a08ffd923ea
vauxhall <zero.griffin@gmail.com>
2017-04-11 11:51:29 -0500

    controller scanner

--- box/Controller/CLanding.swift
+++ box/Controller/CLanding.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CLanding:CController
 {
+    private weak var viewLanding:VLanding!
     
+    override func loadView()
+    {
+        let viewLanding:VLanding = VLanding(controller:self)
+        self.viewLanding = viewLanding
+        view = viewLanding
+    }
 }
---
+++ box/View/Scanner/VScanner.swift
@@ -0,0 +1,9 @@
+//
+//  VScanner.swift
+//  box
+//
+//  Created by zero on 4/11/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
