4e8dc46bdde548d105d50f8718b35cc742f9f173
vauxhall <zero.griffin@gmail.com>
2017-04-15 19:21:09 -0500

    init algo item

--- box/Model/Grid/Algo/MGridAlgoItem.swift
+++ box/Model/Grid/Algo/MGridAlgoItem.swift
@@ -4,4 +4,11 @@ import CoreLocation
 class MGridAlgoItem
 {
     let location:CLLocation
+    
+    init(
+        latitude:Double,
+        longitude:Double)
+    {
+        location = CLLocation(latitude:latitude, longitude:longitude)
+    }
 }
