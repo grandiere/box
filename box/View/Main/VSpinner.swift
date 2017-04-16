7262932bf7eb3ac839c5268aa3c4ea71919f5a35
vauxhall <zero.griffin@gmail.com>
2017-04-16 16:10:08 -0500

    view spinner

--- box/View/Main/VSpinner.swift
+++ box/View/Main/VSpinner.swift
@@ -2,22 +2,17 @@ import UIKit
 
 class VSpinner:UIImageView
 {
-    private let kAnimationDuration:TimeInterval = 5
+    private let kAnimationDuration:TimeInterval = 1.6
     
     init()
     {
         super.init(frame:CGRect.zero)
         
         let images:[UIImage] = [
-            #imageLiteral(resourceName: "assetGenericSpinner0"),
-            #imageLiteral(resourceName: "assetGenericSpinner1"),
-            #imageLiteral(resourceName: "assetGenericSpinner2"),
-            #imageLiteral(resourceName: "assetGenericSpinner3"),
-            #imageLiteral(resourceName: "assetGenericSpinner4"),
-            #imageLiteral(resourceName: "assetGenericSpinner5"),
-            #imageLiteral(resourceName: "assetGenericSpinner6"),
-            #imageLiteral(resourceName: "assetGenericSpinner7"),
-            #imageLiteral(resourceName: "assetGenericSpinner8")
+            #imageLiteral(resourceName: "assetSpinner0"),
+            #imageLiteral(resourceName: "assetSpinner1"),
+            #imageLiteral(resourceName: "assetSpinner2"),
+            #imageLiteral(resourceName: "assetSpinner3")
         ]
         
         isUserInteractionEnabled = false
