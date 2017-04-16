bba0d408cef0c24236d61ffeb3300e3cc19dafff
vauxhall <zero.griffin@gmail.com>
2017-04-15 20:22:16 -0500

    controller- view grid

--- box/Controller/CGrid.swift
+++ box/Controller/CGrid.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CGrid:CController
 {
+    private weak var viewGrid:VGrid!
     
+    override func loadView()
+    {
+        let viewGrid:VGrid = VGrid(controller:self)
+        self.viewGrid = viewGrid
+        view = viewGrid
+    }
 }
