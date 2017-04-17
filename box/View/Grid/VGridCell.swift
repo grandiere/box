7bb137fd2901a22754a3950b6aec4f989587eecd
vauxhall <zero.griffin@gmail.com>
2017-04-17 10:28:50 -0500

    view grid cell layout

--- box/View/Grid/VGridCell.swift
+++ box/View/Grid/VGridCell.swift
@@ -2,5 +2,85 @@ import UIKit
 
 class VGridCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    private weak var label:UILabel!
+    private let attributesTitle:[String:AnyObject]
+    private let attributesSubtitle:[String:AnyObject]
+    private let kImageWidth:CGFloat = 120
+    private let kLabelRight:CGFloat = 10
     
+    override init(frame:CGRect)
+    {
+        attributesTitle = [
+            NSFontAttributeName:UIFont.bolder(size:20),
+            NSForegroundColorAttributeName:UIColor.white]
+        attributesSubtitle = [
+            NSFontAttributeName:UIFont.bold(size:16),
+            NSForegroundColorAttributeName:UIColor(white:1, alpha:0.7)]
+        
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        let label:UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.isUserInteractionEnabled = false
+        label.numberOfLines = 0
+        self.label = label
+        
+        addSubview(imageView)
+        addSubview(label)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:imageView,
+            constant:kImageWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.leftToRight(
+            view:label,
+            toView:imageView)
+        NSLayoutConstraint.rightToRight(
+            view:label,
+            toView:self,
+            constant:kLabelRight)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config(model:MGridItem)
+    {
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        let stringTitle:NSAttributedString = NSAttributedString(
+            string:model.title,
+            attributes:attributesTitle)
+        let stringSubtitle:NSAttributedString = NSAttributedString(
+            string:model.subtitle,
+            attributes:attributesSubtitle)
+        mutableString.append(stringTitle)
+        mutableString.append(stringSubtitle)
+        
+        imageView.image = model.image
+        label.attributedText = mutableString
+    }
 }
