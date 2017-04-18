67585c6fd380c91ac4226a0714471aaa631e3fe4
vauxhall <zero.griffin@gmail.com>
2017-04-18 10:30:04 -0500

    model grid visor orientations

---
+++ box/Model/GridVisor/Orientation/MGridVisorOrientationLandscapeLeft.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class MGridVisorOrientationLandscapeLeft:MGridVisorOrientation
+{
+    override func itemPosition(
+        userHeading:Float,
+        moveVertical:Float,
+        itemHeading:Float) -> MetalPosition?
+    {
+        let normalUserHeading:Float
+        
+        if (itemHeading < kItemMaxThreshold) && (userHeading < 0 && userHeading > kMinThreshold)
+        {
+            normalUserHeading = userHeading * kHorizontalMultiplier
+        }
+        else
+        {
+            normalUserHeading = normalHeading(rawHeading:userHeading)
+        }
+        
+        let positionY:Float = normalUserHeading - itemHeading
+        let positionX:Float = moveVertical / kVerticalDivider
+        
+        let position:MetalPosition = MetalPosition(
+            positionX:positionX,
+            positionY:positionY)
+        
+        return position
+    }
+}
---
+++ box/Model/GridVisor/Orientation/MGridVisorOrientationPortrait.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+class MGridVisorOrientationPortrait:MGridVisorOrientation
+{
+    override func itemPosition(
+        userHeading:Float,
+        moveVertical:Float,
+        itemHeading:Float) -> MetalPosition?
+    {
+        let normalUserHeading:Float
+        
+        if (itemHeading < kItemMaxThreshold) && (userHeading < 0 && userHeading > kMinThreshold)
+        {
+            normalUserHeading = userHeading * kHorizontalMultiplier
+        }
+        else
+        {
+            normalUserHeading = normalHeading(rawHeading:userHeading)
+        }
+        
+        let positionX:Float = itemHeading - normalUserHeading
+        let positionY:Float = moveVertical * kVerticalMultiplier
+        
+        let position:MetalPosition = MetalPosition(
+            positionX:positionX,
+            positionY:positionY)
+        
+        return position
+    }
+}
