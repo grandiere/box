0c9a09d4c203d54d5c1ccc795b6019e72476cf22
vauxhall <zero.griffin@gmail.com>
2017-04-18 15:04:47 -0500

    finder target

--- box/View/GridVisor/VGridVisorTargetFinder.swift
+++ box/View/GridVisor/VGridVisorTargetFinder.swift
@@ -6,11 +6,14 @@ class VGridVisorTargetFinder:UIView
     private var initialRadius:CGFloat
     private var lineTotal:CGFloat
     private let kPi2:CGFloat
+    private let kCircleRadius:CGFloat = 91
     private let kRadius:CGFloat = 100
-    private let kLineWidth:CGFloat = 7
-    private let kLineRadius:CGFloat = 0.07
-    private let kLineSeparation:CGFloat = 0.1
-    private let kRadiusDelta:CGFloat = 0.005
+    private let kEndAngle:CGFloat = 0.0001
+    private let kLineWidth:CGFloat = 10
+    private let kCircleLine:CGFloat = 3
+    private let kLineRadius:CGFloat = 0.03
+    private let kLineSeparation:CGFloat = 0.1352
+    private let kRadiusDelta:CGFloat = 0.009
     
     init(controller:CGridVisor)
     {
@@ -48,9 +51,20 @@ class VGridVisorTargetFinder:UIView
             let height:CGFloat = rect.maxY
             let width_2:CGFloat = width / 2.0
             let height_2:CGFloat = height / 2.0
-            let center:CGPoint = CGPoint(x:width_2, y:height_2)
+            let center:CGPoint = CGPoint(
+                x:width_2,
+                y:height_2)
             
-            context.setLineCap(CGLineCap.round)
+            context.setLineWidth(kCircleLine)
+            context.setStrokeColor(UIColor.gridBlue.cgColor)
+            context.addArc(
+                center:center,
+                radius:kCircleRadius,
+                startAngle:0,
+                endAngle:kEndAngle,
+                clockwise:true)
+            context.drawPath(using:CGPathDrawingMode.stroke)
+            
             context.setLineWidth(kLineWidth)
             context.setStrokeColor(UIColor.gridBlue.cgColor)
             
