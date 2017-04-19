11b9f809233063f9795585b67d13eaadf7e59652
vauxhall <zero.griffin@gmail.com>
2017-04-19 12:18:07 -0500

    view grid visor cell

---
+++ box/View/GridVisorDetail/VGridVisorDetailCell.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class VGridVisorDetailCell:UICollectionViewCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        backgroundColor = UIColor.clear
+        clipsToBounds = true
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config()
+    {
+        
+    }
+}
