b9a8dcd25b46ad1a2b802327e09c6e2940c19414
vauxhall <zero.griffin@gmail.com>
2017-04-20 08:52:46 -0500

    getting heading

--- box/Model/Grid/Algo/MGridAlgoItem.swift
+++ box/Model/Grid/Algo/MGridAlgoItem.swift
@@ -8,6 +8,7 @@ class MGridAlgoItem
     private(set) var heading:Float
     var multipliedHeading:Float
     private let kDistanceDivider:Double = 1000
+    private let k180:Double = 180
     
     init(
         latitude:Double,
@@ -16,6 +17,61 @@ class MGridAlgoItem
         location = CLLocation(latitude:latitude, longitude:longitude)
         heading = 0
         multipliedHeading = 0
+    }
+    
+    //MARK: private
+    
+    private func degreesToRadians(degrees:Double) -> Double
+    {
+        return degrees * Double.pi / k180
+    }
+    
+    private func RadiansToDegrees(_ radians: Double) -> Double {
+        return radians * 180 / M_PI
+    }
+    
+    
+    private func headingFrom(reference:CLLocation) -> Double
+    {
+        let lat1 = DegreesToRadians(self.coordinate.latitude)
+        let lon1 = DegreesToRadians(self.coordinate.longitude)
+        
+        let lat2 = DegreesToRadians(destinationLocation.coordinate.latitude);
+        let lon2 = DegreesToRadians(destinationLocation.coordinate.longitude);
+        
+        let dLon = lon2 - lon1
+        
+        let y = sin(dLon) * cos(lat2);
+        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
+        let radiansBearing = atan2(y, x)
+        
+        return radiansBearing
+    }
+    
+    func bearingToLocationDegrees(destinationLocation:CLLocation) -> Double{
+        return   RadiansToDegrees(bearingToLocationRadian(destinationLocation))
+    }
+    
+    private func getHeading(reference:CLLocationCoordinate2D) -> Float
+    {
+        let degToRad:Double = Double.pi / k180
+        let piReference:Double = reference.latitude * degToRad
+        let piLocation:Double = location.coordinate.latitude * degToRad
+        let lambdaReference:Double = reference.longitude * degToRad
+        let lambdaLocation:Double = location.coordinate.longitude * degToRad
+        
+        let deltaLambda:Double = lambdaLocation - lambdaReference
+        let sinPiReference:Double = sin(piReference)
+        let sinPiLocation:Double = sin(piLocation)
+        let cosPiReference:Double = cos(piReference)
+        let cosPiLocation:Double = cos(piLocation)
+        let cosLambda:Double = cos(deltaLambda)
+        
+        let sumDistance:Double = sinPiReference * sinPiLocation + cosPiReference * cosPiLocation * cosLambda
+        let angularDistance:Double = acos(sumDistance)
+        let distance = earthRadius * angularDistance
+        
+        return distance
     }
     
     //MARK: public
