81315197f7650271ac78465292be12d07552dc8a
vauxhall <zero.griffin@gmail.com>
2017-04-20 09:19:46 -0500

    heading from location

--- box/Model/Grid/Algo/MGridAlgoItem.swift
+++ box/Model/Grid/Algo/MGridAlgoItem.swift
@@ -26,52 +26,38 @@ class MGridAlgoItem
         return degrees * Double.pi / k180
     }
     
-    private func RadiansToDegrees(_ radians: Double) -> Double {
-        return radians * 180 / M_PI
-    }
-    
-    
-    private func headingFrom(reference:CLLocation) -> Double
+    private func radiansToDegrees(radians:Double) -> Double
     {
-        let lat1 = DegreesToRadians(self.coordinate.latitude)
-        let lon1 = DegreesToRadians(self.coordinate.longitude)
-        
-        let lat2 = DegreesToRadians(destinationLocation.coordinate.latitude);
-        let lon2 = DegreesToRadians(destinationLocation.coordinate.longitude);
-        
-        let dLon = lon2 - lon1
-        
-        let y = sin(dLon) * cos(lat2);
-        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon);
-        let radiansBearing = atan2(y, x)
-        
-        return radiansBearing
+        return radians * k180 / Double.pi
     }
     
-    func bearingToLocationDegrees(destinationLocation:CLLocation) -> Double{
-        return   RadiansToDegrees(bearingToLocationRadian(destinationLocation))
-    }
-    
-    private func getHeading(reference:CLLocationCoordinate2D) -> Float
+    private func headingFrom(reference:CLLocationCoordinate2D) -> Float
     {
-        let degToRad:Double = Double.pi / k180
-        let piReference:Double = reference.latitude * degToRad
-        let piLocation:Double = location.coordinate.latitude * degToRad
-        let lambdaReference:Double = reference.longitude * degToRad
-        let lambdaLocation:Double = location.coordinate.longitude * degToRad
+        let referenceLatitude:Double = degreesToRadians(
+            degrees:reference.latitude)
+        let referenceLongitude:Double = degreesToRadians(
+            degrees:reference.longitude)
+        let latitude:Double = degreesToRadians(
+            degrees:location.coordinate.latitude)
+        let longitude:Double = degreesToRadians(
+            degrees:location.coordinate.longitude)
         
-        let deltaLambda:Double = lambdaLocation - lambdaReference
-        let sinPiReference:Double = sin(piReference)
-        let sinPiLocation:Double = sin(piLocation)
-        let cosPiReference:Double = cos(piReference)
-        let cosPiLocation:Double = cos(piLocation)
-        let cosLambda:Double = cos(deltaLambda)
+        let deltaLongitude:Double = longitude - referenceLongitude
+        let sinDeltaLongitude:Double = sin(deltaLongitude)
+        let cosLatitude:Double = cos(latitude)
+        let cosReferenceLatitude:Double = cos(referenceLatitude)
+        let sinLatitude:Double = sin(latitude)
+        let sinReferenceLatitude:Double = sin(referenceLatitude)
+        let cosDeltaLongitude:Double = cos(deltaLongitude)
         
-        let sumDistance:Double = sinPiReference * sinPiLocation + cosPiReference * cosPiLocation * cosLambda
-        let angularDistance:Double = acos(sumDistance)
-        let distance = earthRadius * angularDistance
+        let yPoint:Double = sinDeltaLongitude * cosLatitude
+        let xPoint:Double = cosReferenceLatitude * sinLatitude - sinReferenceLatitude * cosLatitude * cosDeltaLongitude
+        let radiansBearing:Double = atan2(yPoint, xPoint)
+        let degreesBearing:Double = radiansToDegrees(
+            radians:radiansBearing)
+        let floatBearing:Float = Float(degreesBearing)
         
-        return distance
+        return floatBearing
     }
     
     //MARK: public
@@ -79,6 +65,9 @@ class MGridAlgoItem
     final func distanceUser(userLocation:CLLocation)
     {
         distance = location.distance(from:userLocation) / kDistanceDivider
+        heading = headingFrom(reference:userLocation.coordinate)
+        
+        print(heading)
     }
     
     func imageStandby() -> UIImage?
