31b5cbccc465e58749e7e00f90be8fd2bbd592da
vauxhall <zero.griffin@gmail.com>
2017-04-21 09:00:19 -0500

    view landing bar

---
+++ box/View/Landing/VLandingBar.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class VLandingBar:UIView
+{
+    private weak var controller:CLanding!
+    
+    init(controller:CLanding)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
--- box/View/Landing/VLandingCell.swift
+++ box/View/Landing/VLandingCell.swift
@@ -2,5 +2,22 @@ import UIKit
 
 class VLandingCell:UICollectionViewCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config()
+    {
+        
+    }
 }
