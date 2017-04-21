954202bb5ae23509314a15cbc72e91ccd5d730f6
vauxhall <zero.griffin@gmail.com>
2017-04-21 15:03:46 -0500

    view profile cell circle

--- box/View/Profile/VProfileCell.swift
+++ box/View/Profile/VProfileCell.swift
@@ -1,9 +1,12 @@
 import UIKit
 
-class VProfileCell:UICollectionViewCell
+class VProfileCell:UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
+    private weak var model:MProfileItem?
     private weak var label:UILabel!
+    private weak var collectionView:VCollection!
     private let kLabelLeft:CGFloat = 10
+    private let kCircleWidth:CGFloat = 30
     private let kLabelWidth:CGFloat = 100
     
     override init(frame:CGRect)
@@ -43,6 +46,39 @@ class VProfileCell:UICollectionViewCell
     
     func config(model:MProfileItem)
     {
+        self.model = model
         label.text = model.title
+        collectionView.reloadData()
+    }
+    
+    //MARK: collectionView delegate
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        guard
+        
+            let count:Int = model?.value
+        
+        else
+        {
+            return 0
+        }
+        
+        return count
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath)
+    {
+        let cell:VProfileCellCircle
     }
 }
---
+++ box/View/Profile/VProfileCellCircle.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class VProfileCellCircle:UICollectionViewCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        isUserInteractionEnabled = false
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
