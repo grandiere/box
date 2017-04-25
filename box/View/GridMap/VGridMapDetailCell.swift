02f4b4449c08af4054a349483d8cfcddb586d810
vauxhall <zero.griffin@gmail.com>
2017-04-25 11:59:01 -0500

    detail cell label

--- box/View/GridMap/VGridMapDetailCell.swift
+++ box/View/GridMap/VGridMapDetailCell.swift
@@ -2,11 +2,31 @@ import UIKit
 
 class VGridMapDetailCell:UICollectionViewCell
 {
+    private weak var label:UILabel!
+    private let kLabelMargin:CGFloat = 10
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.backgroundColor = UIColor.clear
+        self.label = label
+        
+        addSubview(label)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:label,
+            toView:self,
+            margin:kLabelMargin)
     }
     
     required init?(coder:NSCoder)
@@ -18,6 +38,6 @@ class VGridMapDetailCell:UICollectionViewCell
     
     func config(model:MGridMapDetailItem)
     {
-        
+        label.attributedText = model.attributedString
     }
 }
