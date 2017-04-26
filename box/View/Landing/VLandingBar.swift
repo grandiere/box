1cdf44bef31575513b82e750a5dcb3bc35d04247
vauxhall <zero.griffin@gmail.com>
2017-04-26 14:39:46 -0500

    landing bar collection view layout

--- box/View/Landing/VLandingBar.swift
+++ box/View/Landing/VLandingBar.swift
@@ -1,10 +1,12 @@
 import UIKit
 
-class VLandingBar:UIView
+class VLandingBar:UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CLanding!
+    private weak var collectionView:VCollection!
     private let kBorderHeight:CGFloat = 1
     private let kBlurAlpha:CGFloat = 0.99
+    private let kCellWidth:CGFloat = 60
     
     init(controller:CLanding)
     {
@@ -24,9 +26,23 @@ class VLandingBar:UIView
         
         let border:VBorder = VBorder(color:UIColor(white:1, alpha:0.1))
         
+        let collectionView:VCollection = VCollection()
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(cell:VLandingBarCell.self)
+        self.collectionView = collectionView
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        }
+        
         blurContainer.addSubview(blur)
         addSubview(blurContainer)
         addSubview(border)
+        addSubview(collectionView)
         
         NSLayoutConstraint.equals(
             view:blurContainer,
@@ -45,10 +61,57 @@ class VLandingBar:UIView
         NSLayoutConstraint.equalsHorizontal(
             view:border,
             toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: private
+    
+    private func modelAtIndex(index:IndexPath) -> MLandingBarProtocol
+    {
+        let item:MLandingBarProtocol = controller.model.barItems[index.item]
+        
+        return item
+    }
+    
+    //MARK: collectionView delegate
+    
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        let height:CGFloat = collectionView.bounds.maxY
+        let size:CGSize = CGSize(width:kCellWidth, height:height)
+        
+        return size
+    }
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.barItems.count
+        
+        return count
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let item:MLandingBarProtocol = modelAtIndex(index:indexPath)
+        let cell:VLandingBarCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier:
+            VLandingBarCell.reusableIdentifier,
+            for:indexPath) as! VLandingBarCell
+        cell.config(model:item)
+        
+        return cell
     }
 }
--- box/View/Landing/VLandingBarCell.swift
+++ box/View/Landing/VLandingBarCell.swift
@@ -2,5 +2,35 @@ import UIKit
 
 class VLandingBarCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config(model:MLandingBarProtocol)
+    {
+        imageView.image = model.image
+    }
 }
