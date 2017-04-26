5880ba282688c6c5b32cd7bee169d9c796e7db9a
vauxhall <zero.griffin@gmail.com>
2017-04-26 12:58:13 -0500

    view landing bar cell

--- box/View/Settings/VSettings.swift
+++ box/View/Settings/VSettings.swift
@@ -27,6 +27,11 @@ class VSettings:VView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
     
     func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
     {
-        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        return UICollectionViewCell()
     }
 }
