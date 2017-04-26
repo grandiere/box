5e969a5199f29513ef073358c378027618d6a08d
vauxhall <zero.griffin@gmail.com>
2017-04-26 16:41:17 -0500

    model landing bar store

---
+++ box/Model/Landing/MLandingBarStore.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class MLandingBarStore:MLandingBarProtocol
+{
+    var image:UIImage
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetGenericStore")
+        }
+    }
+    
+    //MARK: landingBar protocol
+    
+    func selected(controller:CLanding)
+    {
+    }
+}
