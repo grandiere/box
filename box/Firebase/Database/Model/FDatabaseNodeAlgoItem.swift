05eda2602187e0b08e2e071fd163e8a414aa9bf7
vauxhall <zero.griffin@gmail.com>
2017-04-27 17:57:22 -0500

    algo item

--- box/Firebase/Database/Model/FDatabaseNodeAlgoItem.swift
+++ box/Firebase/Database/Model/FDatabaseNodeAlgoItem.swift
@@ -2,5 +2,52 @@ import Foundation
 
 class FDatabaseNodeAlgoItem:FDatabaseNodeProtocol
 {
+    static let latitude:String = "latitude"
+    static let longitude:String = "longitude"
+    static let created:String = "created"
     
+    let latitude:Double
+    let longitude:Double
+    let created:TimeInterval
+    
+    init(
+        latitude:Double,
+        longitude:Double,
+        created:TimeInterval)
+    {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.created = created
+    }
+    
+    //MARK: node protocol
+    
+    required init?(snapshot:Any)
+    {
+        guard
+            
+            let snapshotDict:[String:Any] = snapshot as? [String:Any],
+            let latitude:Double = snapshotDict[FDatabaseNodeAlgoItem.latitude] as? Double,
+            let longitude:Double = snapshotDict[FDatabaseNodeAlgoItem.longitude] as? Double,
+            let created:TimeInterval = snapshotDict[FDatabaseNodeAlgoItem.created] as? TimeInterval
+        
+        else
+        {
+            return nil
+        }
+        
+        self.latitude = latitude
+        self.longitude = longitude
+        self.created = created
+    }
+    
+    func json() -> Any?
+    {
+        var json:[String:Any] = [
+            FDatabaseNodeAlgoItem.latitude:latitude,
+            FDatabaseNodeAlgoItem.longitude:longitude,
+            FDatabaseNodeAlgoItem.created:created]
+        
+        return json
+    }
 }
