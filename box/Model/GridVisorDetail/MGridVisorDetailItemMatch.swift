a447706f6840695457d2a2afa7a01f6df1f8fdbd
vauxhall <zero.griffin@gmail.com>
2017-04-29 13:58:41 -0500

    refactor debug for match

--- box/Model/GridVisorDetail/MGridVisorDetailItemMatch.swift
+++ box/Model/GridVisorDetail/MGridVisorDetailItemMatch.swift
@@ -1,13 +1,13 @@
 import UIKit
 
-class MGridVisorDetailItemDebug:MGridVisorDetailItem
+class MGridVisorDetailItemMatch:MGridVisorDetailItem
 {
     let credits:Int
     private let kCellHeight:CGFloat = 280
     
     init(model:MGridAlgoItemHostile)
     {
-        let reusableIdentifier:String = VGridVisorDetailCellDebug.reusableIdentifier
+        let reusableIdentifier:String = VGridVisorDetailCellMatch.reusableIdentifier
         credits = model.credits
         
         super.init(
