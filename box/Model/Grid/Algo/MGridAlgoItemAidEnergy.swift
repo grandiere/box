def8c4a96fe30143d69cb63a5e8c60defd157a29
vauxhall <zero.griffin@gmail.com>
2017-04-29 12:00:20 -0500

    only one type of aid

--- box/Model/Grid/Algo/MGridAlgoItemAidEnergy.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-class MGridAlgoItemAidEnergy:MGridAlgoItemAid
-{
-    
-}
--- box/Model/Grid/Algo/MGridAlgoItemAidStats.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MGridAlgoItemAidStats.swift
-//  box
-//
-//  Created by zero on 4/29/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
-import Foundation
