2e6f229fcebe27b83baa32197f9c8af44557b8f1
vauxhall <zero.griffin@gmail.com>
2017-04-29 14:15:37 -0500

    view cell take

--- box/View/GridVisorDetail/VGridVisorDetailCellTake.swift
+++ box/View/GridVisorDetail/VGridVisorDetailCellTake.swift
@@ -2,92 +2,69 @@ import UIKit
 
 class VGridVisorDetailCellTake:VGridVisorDetailCell
 {
-    private weak var modelTake:MGridVisorDetailItemTake?
-    private let stringDebugTitle:NSAttributedString
-    private let stringEnergyTitle:NSAttributedString
-    private let attributesEnergy:[String:AnyObject]
     private let kButtonTop:CGFloat = 10
     private let kButtonHeight:CGFloat = 90
     private let kButtonWidth:CGFloat = 80
     private let kLabelWidth:CGFloat = 200
-    private let kAlphaActive:CGFloat = 1
-    private let kAlphaInactive:CGFloat = 0.2
     
     override init(frame:CGRect)
     {
-        let attributesDebugTitle:[String:AnyObject] = [
-            NSFontAttributeName:UIFont.bold(size:15),
-            NSForegroundColorAttributeName:UIColor.black]
-        let attributesEnergyTitle:[String:AnyObject] = [
-            NSFontAttributeName:UIFont.regular(size:13),
-            NSForegroundColorAttributeName:UIColor(white:0, alpha:0.8)]
-        attributesEnergy = [
-            NSFontAttributeName:UIFont.numeric(size:30),
-            NSForegroundColorAttributeName:UIColor.black]
-        
-        stringDebugTitle = NSAttributedString(
-            string:NSLocalizedString("VGridVisorDetailCellDebug_labelTitle", comment:""),
-            attributes:attributesDebugTitle)
-        stringEnergyTitle = NSAttributedString(
-            string:NSLocalizedString("VGridVisorDetailCellDebug_energyTitle", comment:""),
-            attributes:attributesEnergyTitle)
-        
         super.init(frame:frame)
         backgroundColor = UIColor.clear
         
-        let labelEnergy:UILabel = UILabel()
-        labelEnergy.translatesAutoresizingMaskIntoConstraints = false
-        labelEnergy.backgroundColor = UIColor.clear
-        labelEnergy.isUserInteractionEnabled = false
-        labelEnergy.textAlignment = NSTextAlignment.right
-        labelEnergy.numberOfLines = 0
-        self.labelEnergy = labelEnergy
+        let labelTitle:UILabel = UILabel()
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.textAlignment = NSTextAlignment.right
+        labelTitle.font = UIFont.bold(size:15)
+        labelTitle.textColor = UIColor.black
+        labelTitle.text = NSLocalizedString("VGridVisorDetailCellTake_labelTitle", comment:"")
         
-        let buttonDebug:UIButton = UIButton()
-        buttonDebug.translatesAutoresizingMaskIntoConstraints = false
-        buttonDebug.setImage(
+        let buttonTake:UIButton = UIButton()
+        buttonTake.translatesAutoresizingMaskIntoConstraints = false
+        buttonTake.setImage(
             #imageLiteral(resourceName: "assetGenericMatch").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
             for:UIControlState.normal)
-        buttonDebug.setImage(
+        buttonTake.setImage(
             #imageLiteral(resourceName: "assetGenericMatchOver").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
             for:UIControlState.highlighted)
-        buttonDebug.imageView!.clipsToBounds = true
-        buttonDebug.imageView!.contentMode = UIViewContentMode.center
-        buttonDebug.addTarget(
+        buttonTake.imageView!.clipsToBounds = true
+        buttonTake.imageView!.contentMode = UIViewContentMode.center
+        buttonTake.addTarget(
             self,
-            action:#selector(actionEnter(sender:)),
+            action:#selector(actionTake(sender:)),
             for:UIControlEvents.touchUpInside)
-        self.buttonDebug = buttonDebug
         
-        addSubview(labelEnergy)
-        addSubview(buttonDebug)
+        addSubview(labelTitle)
+        addSubview(buttonTake)
         
         NSLayoutConstraint.topToTop(
-            view:labelEnergy,
+            view:labelTitle,
             toView:self,
             constant:kButtonTop)
         NSLayoutConstraint.height(
-            view:labelEnergy,
+            view:labelTitle,
             constant:kButtonHeight)
         NSLayoutConstraint.rightToLeft(
-            view:labelEnergy,
-            toView:buttonDebug)
+            view:labelTitle,
+            toView:buttonTake)
         NSLayoutConstraint.width(
-            view:labelEnergy,
+            view:labelTitle,
             constant:kLabelWidth)
         
         NSLayoutConstraint.topToTop(
-            view:buttonDebug,
+            view:buttonTake,
             toView:self,
             constant:kButtonTop)
         NSLayoutConstraint.height(
-            view:buttonDebug,
+            view:buttonTake,
             constant:kButtonHeight)
         NSLayoutConstraint.rightToRight(
-            view:buttonDebug,
+            view:buttonTake,
             toView:self)
         NSLayoutConstraint.width(
-            view:buttonDebug,
+            view:buttonTake,
             constant:kButtonWidth)
     }
     
@@ -96,56 +73,9 @@ class VGridVisorDetailCellTake:VGridVisorDetailCell
         return nil
     }
     
-    override func config(controller:CGridVisorDetail, model:MGridVisorDetailItem)
-    {
-        super.config(controller:controller, model:model)
-        
-        guard
-            
-            let modelDebug:MGridVisorDetailItemDebug = model as? MGridVisorDetailItemDebug
-            
-            else
-        {
-            return
-        }
-        
-        self.modelDebug = modelDebug
-        let credits:Int = modelDebug.credits
-        let rawString:String = "\(credits)"
-        let stringCredits:NSAttributedString = NSAttributedString(
-            string:rawString,
-            attributes:attributesEnergy)
-        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
-        mutableString.append(stringDebugTitle)
-        mutableString.append(stringEnergyTitle)
-        mutableString.append(stringCredits)
-        
-        labelEnergy.attributedText = mutableString
-        
-        guard
-            
-            let energy:Int = MSession.sharedInstance.settings?.energy?.percentEnergy()
-            
-            else
-        {
-            return
-        }
-        
-        if credits > energy
-        {
-            buttonDebug.isUserInteractionEnabled = false
-            buttonDebug.alpha = kAlphaInactive
-        }
-        else
-        {
-            buttonDebug.isUserInteractionEnabled = true
-            buttonDebug.alpha = kAlphaActive
-        }
-    }
-    
     //MARK: actions
     
-    func actionEnter(sender button:UIButton)
+    func actionTake(sender button:UIButton)
     {
         controller?.enterMach()
     }
