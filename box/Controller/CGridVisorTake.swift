b7992995e88fbb45d37178594bd5a23b4737105d
vauxhall <zero.griffin@gmail.com>
2017-04-30 14:01:35 -0500

    controller-view take

--- box/Controller/CGridVisorTake.swift
+++ box/Controller/CGridVisorTake.swift
@@ -2,5 +2,61 @@ import UIKit
 
 class CGridVisorTake:CController
 {
+    private var modelTake:MGridVisorTake?
+    private(set) weak var model:MGridAlgoItemAid!
+    private weak var viewTake:VGridVisorTake!
     
+    init(model:MGridAlgoItemAid)
+    {
+        self.model = model
+        super.init()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func viewDidAppear(_ animated:Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        if modelTake == nil
+        {
+            let controllersCount:Int = parentController.childViewControllers.count
+            let prevController:Int = controllersCount - 2
+            
+            if prevController > 0
+            {
+                if let _:CGridVisorDetail = parentController.childViewControllers[prevController] as? CGridVisorDetail
+                {
+                    parentController.popSilent(removeIndex:prevController)
+                }
+            }
+            
+            modelTake = MGridVisorTake()
+        }
+        
+        parentController.viewParent.panRecognizer.isEnabled = false
+    }
+    
+    override func loadView()
+    {
+        let viewTake:VGridVisorTake = VGridVisorTake(controller:self)
+        self.viewTake = viewTake
+        view = viewTake
+    }
+    
+    //MARK: private
+    
+    private func finishMatch()
+    {
+        DispatchQueue.main.async
+        { [weak self] in
+                
+            self?.parentController.dismissAnimateOver(completion:nil)
+        }
+    }
+    
+    //MARK: public
 }
--- box/View/GridVisorTake/VGridVisorTake.swift
+++ box/View/GridVisorTake/VGridVisorTake.swift
@@ -2,5 +2,16 @@ import UIKit
 
 class VGridVisorTake:VView
 {
+    private weak var controller:CGridVisorTake!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CGridVisorTake
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
