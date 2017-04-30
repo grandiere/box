211dcde3253a69a0c1162abfa2049927bec35a13
vauxhall <zero.griffin@gmail.com>
2017-04-30 12:58:49 -0500

    button close on detail map

--- box/View/GridMap/VGridMapDetailHeader.swift
+++ box/View/GridMap/VGridMapDetailHeader.swift
@@ -4,6 +4,7 @@ class VGridMapDetailHeader:UICollectionReusableView
 {
     private weak var imageView:UIImageView!
     private let kImageTop:CGFloat = 10
+    private let kButtonSize:CGFloat = 60
     
     override init(frame:CGRect)
     {
@@ -18,7 +19,24 @@ class VGridMapDetailHeader:UICollectionReusableView
         imageView.contentMode = UIViewContentMode.scaleAspectFit
         self.imageView = imageView
         
+        let buttonClose:UIButton = UIButton()
+        buttonClose.translatesAutoresizingMaskIntoConstraints = false
+        buttonClose.setImage(
+            #imageLiteral(resourceName: "assetGenericClose").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        buttonClose.setImage(
+            #imageLiteral(resourceName: "assetGenericClose").withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        buttonClose.imageView!.tintColor = UIColor(white:0, alpha:0.2)
+        buttonClose.imageView!.clipsToBounds = true
+        buttonClose.imageView!.contentMode = UIViewContentMode.center
+        buttonClose.addTarget(
+            self,
+            action:#selector(actionClose(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
         addSubview(imageView)
+        addSubview(buttonClose)
         
         NSLayoutConstraint.topToTop(
             view:imageView,
@@ -30,11 +48,28 @@ class VGridMapDetailHeader:UICollectionReusableView
         NSLayoutConstraint.equalsHorizontal(
             view:imageView,
             toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:buttonClose,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:buttonClose,
+            toView:self)
+        NSLayoutConstraint.size(
+            view:buttonClose,
+            constant:kButtonSize)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: actions
+    
+    func actionClose(sender button:UIButton)
+    {
+        
     }
     
     //MARK: public
