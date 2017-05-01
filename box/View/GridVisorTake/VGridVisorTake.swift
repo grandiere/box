4ba812b12778c756dafaae02ed0c19254adee7e1
vauxhall <zero.griffin@gmail.com>
2017-05-01 15:31:43 -0500

    view take image aid

--- box/View/GridVisorTake/VGridVisorTake.swift
+++ box/View/GridVisorTake/VGridVisorTake.swift
@@ -3,11 +3,39 @@ import UIKit
 class VGridVisorTake:VView
 {
     private weak var controller:CGridVisorTake!
+    private let kImageHeight:CGFloat = 180
     
     override init(controller:CController)
     {
         super.init(controller:controller)
+        backgroundColor = UIColor.clear
         self.controller = controller as? CGridVisorTake
+        
+        let blur:VBlur = VBlur.light()
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.isUserInteractionEnabled = false
+        imageView.image = #imageLiteral(resourceName: "assetTextureAidDetail")
+        
+        addSubview(blur)
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:blur,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:imageView,
+            constant:kImageHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:imageView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
