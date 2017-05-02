dcff81a81bf78a9592168ec20d4e64fdde9a3e26
vauxhall <zero.griffin@gmail.com>
2017-05-02 08:52:34 -0500

    grid visor take skill

---
+++ box/Model/GridVisorTake/MGridVisorTakeSkill.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class MGridVisorTakeSkill:MGridVisorTakeProtocol
+{
+    //MARK: visorTake protocol
+    
+    var descr:NSAttributedString
+    {
+        get
+        {
+            return NSAttributedString()
+        }
+    }
+    
+    func apply(controller:CGridVisorTake)
+    {
+        
+    }
+}
