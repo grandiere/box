590aded0e669f3948ad868facedb9a95c4fd398f
vauxhall <zero.griffin@gmail.com>
2017-05-02 08:50:40 -0500

    grid visor take energy

---
+++ box/Model/GridVisorTake/MGridVisorTakeEnergy.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class MGridVisorTakeEnergy:MGridVisorTakeProtocol
+{
+    //MARK: visorTake protocol
+    
+    var descr:NSAttributedString
+    {
+        get
+        {
+            return NSAttributedString()
+        }
+    }
+    
+    func apply(controller:CGridVisorTake)
+    {
+        
+    }
+}
