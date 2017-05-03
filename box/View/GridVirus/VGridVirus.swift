987378ed6502b8facd5603a536bd787dc487cb1d
vauxhall <zero.griffin@gmail.com>
2017-05-03 14:10:57 -0500

    view virus released

--- box/View/GridVirus/VGridVirus.swift
+++ box/View/GridVirus/VGridVirus.swift
@@ -5,13 +5,26 @@ class VGridVirus:VView
     private weak var controller:CGridVirus!
     private weak var layoutLandingTop:NSLayoutConstraint!
     private weak var layoutLandingHeight:NSLayoutConstraint!
+    private let kAnimationDuration:TimeInterval = 0.3
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CGridVirus
         
+        let viewLanding:VGridVirusLanding = VGridVirusLanding(
+            controller:self.controller)
         
+        addSubview(viewLanding)
+        
+        layoutLandingTop = NSLayoutConstraint.topToTop(
+            view:viewLanding,
+            toView:self)
+        layoutLandingHeight = NSLayoutConstraint.height(
+            view:viewLanding)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewLanding,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -21,7 +34,22 @@ class VGridVirus:VView
     
     override func layoutSubviews()
     {
+        let height:CGFloat = bounds.maxY
+        layoutLandingHeight.constant = height
         
         super.layoutSubviews()
+    }
+    
+    //MARK: public
+    
+    func animateRelease()
+    {
+        layoutLandingTop.constant = -layoutLandingHeight.constant
+        
+        UIView.animate(withDuration:kAnimationDuration)
+        { [weak self] in
+            
+            self?.layoutIfNeeded()
+        }
     }
 }
