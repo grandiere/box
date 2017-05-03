a317e5f4292dc0c2f4065d6f20ee6fc1433cb1cc
vauxhall <zero.griffin@gmail.com>
2017-05-02 19:27:45 -0500

    controller/view virus

--- box/Controller/CGridVirus.swift
+++ box/Controller/CGridVirus.swift
@@ -2,5 +2,24 @@ import UIKit
 
 class CGridVirus:CController
 {
+    private weak var model:MGridAlgo!
+    private weak var viewVirus:VGridVirus!
     
+    init(model:MGridAlgo)
+    {
+        self.model = model
+        super.init()
+    }
+    
+    required init?(coder: NSCoder)
+    {
+        return nil
+    }
+    
+    override func loadView()
+    {
+        let viewVirus:VGridVirus = VGridVirus(controller:self)
+        self.viewVirus = viewVirus
+        view = viewVirus
+    }
 }
--- box/View/GridVirus/VGridVirus.swift
+++ box/View/GridVirus/VGridVirus.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class VGridVirus:VView
 {
+    private weak var controller:CGridVirus!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        
+        self.controller = controller as? CGridVirus
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
