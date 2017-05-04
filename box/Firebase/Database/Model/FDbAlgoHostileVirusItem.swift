0d9bce7b14c3acf948bf51774a17d5f044329e41
vauxhall <zero.griffin@gmail.com>
2017-05-03 19:08:42 -0500

    fdb virus item

--- box/Firebase/Database/Model/FDbAlgoHostileVirusItem.swift
+++ box/Firebase/Database/Model/FDbAlgoHostileVirusItem.swift
@@ -1,9 +1,5 @@
-//
-//  FDbAlgoHostileVirusItem.swift
-//  box
-//
-//  Created by zero on 5/3/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class FDbAlgoHostileVirusItem:FDbAlgoHostileItem
+{
+}
