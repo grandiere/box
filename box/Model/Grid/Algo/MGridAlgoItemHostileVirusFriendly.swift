f4e4286aebdf412edcb5aeaeae24c905f4ea9939
vauxhall <zero.griffin@gmail.com>
2017-05-04 12:17:19 -0500

    texture virus

--- box/Model/Grid/Algo/MGridAlgoItemHostileVirusFriendly.swift
+++ box/Model/Grid/Algo/MGridAlgoItemHostileVirusFriendly.swift
@@ -1,5 +1,19 @@
-import Foundation
+import UIKit
 
 class MGridAlgoItemHostileVirusFriendly:MGridAlgoItemHostile
 {
+    override func imageStandby() -> UIImage?
+    {
+        return #imageLiteral(resourceName: "assetTextureVirusFriendlyStandBy")
+    }
+    
+    override func imageTargeted() -> UIImage?
+    {
+        return #imageLiteral(resourceName: "assetTextureVirusFriendlyTargeted")
+    }
+    
+    override func imageDetail() -> UIImage?
+    {
+        return #imageLiteral(resourceName: "assetTextureVirusFriendlyDetail")
+    }
 }
