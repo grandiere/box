70a15ea6bf3767bef0bb7939ddbe52ec9d783649
vauxhall <zero.griffin@gmail.com>
2017-05-07 17:03:51 -0500

    controller - view tutorial

--- box/Controller/CTutorial.swift
+++ box/Controller/CTutorial.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CTutorial:CController
 {
+    private weak var viewTutorial:VTutorial!
     
+    override func loadView()
+    {
+        let viewTutorial:VTutorial = VTutorial(controller:self)
+        self.viewTutorial = viewTutorial
+        view = viewTutorial
+    }
 }
