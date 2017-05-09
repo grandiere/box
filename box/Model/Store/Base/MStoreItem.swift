f54801cf53fea77a725d3afeeaaf97201b5c5a5a
vauxhall <zero.griffin@gmail.com>
2017-05-09 09:05:53 -0500

    removed store protocol

--- box/Model/Store/Base/MStoreItem.swift
+++ box/Model/Store/Base/MStoreItem.swift
@@ -1,6 +1,70 @@
 import Foundation
+import StoreKit
 
-class MStoreItem:MStoreProtocol
+class MStoreItem
 {
+    let purchaseId:String
+    let title:String
+    let descr:String
+    var skProduct:SKProduct?
+    private(set) var price:String?
+    private(set) var status:MStoreStatusProtocol?
     
+    //MARK: public
+    
+    func purchaseAction()
+    {
+    }
+    
+    func validatePurchase() -> Bool
+    {
+        return false
+    }
+    
+    //MARK: final
+    
+    final func foundPurchase(price:String)
+    {
+        self.price = price
+        
+        let isPurchased:Bool = validatePurchase()
+        
+        if isPurchased
+        {
+            statusPurchased(callAction:false)
+        }
+        else
+        {
+            statusNew()
+        }
+    }
+    
+    final func statusNew()
+    {
+        status = MStoreItemStatusNew()
+    }
+    
+    final func statusDeferred()
+    {
+        status = MStoreItemStatusDeferred()
+    }
+    
+    final func statusPurchasing()
+    {
+        status = MStoreItemStatusPurchasing()
+    }
+    
+    final func statusPurchased(callAction:Bool)
+    {
+        status = MStoreItemStatusPurchased()
+        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+            { [weak self] in
+                
+                if callAction
+                {
+                    self?.purchaseAction()
+                }
+        }
+    }
 }
