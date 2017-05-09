40de7c3727308b6a9536e80ed0e17fce31550c1e
vauxhall <zero.griffin@gmail.com>
2017-05-09 15:23:43 -0500

    settings cell distance

--- box/View/Settings/VSettingsCell.swift
+++ box/View/Settings/VSettingsCell.swift
@@ -2,5 +2,15 @@ import UIKit
 
 class VSettingsCell:UICollectionViewCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
---
+++ box/View/Settings/VSettingsCellDistance.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class VSettingsCellDistance:VSettingsCell
+{
+    private weak var segmented:UISegmentedControl!
+    private let kLabelLeft:CGFloat = 10
+    private let kLabelWidth:CGFloat = 200
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.font = UIFont.regular(size:15)
+        labelTitle.textColor = UIColor.white
+        labelTitle.text = NSLocalizedString("VSettingsCellDistance_labelTitle", comment:"")
+        
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:labelTitle,
+            toView:self,
+            constant:kLabelLeft)
+        NSLayoutConstraint.width(
+            view:labelTitle,
+            constant:kLabelWidth)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
