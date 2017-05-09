145ac69b6ecab7a92e3eaec38ddbcee7ad5049f5
vauxhall <zero.griffin@gmail.com>
2017-05-09 14:48:32 -0500

    collection view in settings

--- box/View/Settings/VSettings.swift
+++ box/View/Settings/VSettings.swift
@@ -4,12 +4,34 @@ class VSettings:VView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
 {
     private weak var controller:CSettings!
     private weak var collectionView:VCollection!
+    private let kHeaderHeight:CGFloat = 200
+    private let kCollectionBottom:CGFloat = 20
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         clipsToBounds = true
         self.controller = controller as? CSettings
+        
+        let collectionView:VCollection = VCollection()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(cell:VSettingsCell.self)
+        collectionView.registerHeader(header:VSettingsHeader.self)
+        self.collectionView = collectionView
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.headerReferenceSize = CGSize(width:0, height:kHeaderHeight)
+        }
+        
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -27,6 +49,18 @@ class VSettings:VView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
     func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
     {
         return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, viewForSupplementaryElementOfKind kind:String, at indexPath:IndexPath) -> UICollectionReusableView
+    {
+        let header:VSettingsHeader = collectionView.dequeueReusableSupplementaryView(
+            ofKind:kind,
+            withReuseIdentifier:
+            VSettingsHeader.reusableIdentifier,
+            for:indexPath) as! VSettingsHeader
+        header.config(controller:controller)
+        
+        return header
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
--- box/View/Settings/VSettingsHeader.swift
+++ box/View/Settings/VSettingsHeader.swift
@@ -2,18 +2,44 @@ import UIKit
 
 class VSettingsHeader:UICollectionReusableView
 {
+    private weak var controller:CSettings?
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
-        isUserInteractionEnabled = false
         backgroundColor = UIColor.clear
         
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "assetTextureVirusFriendlyTargeted")
         
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: actions
+    
+    func actionBack(sender button:UIButton)
+    {
+        controller?.back()
+    }
+    
+    //MARK: public
+    
+    func config(controller:CSettings)
+    {
+        self.controller = controller
     }
 }
