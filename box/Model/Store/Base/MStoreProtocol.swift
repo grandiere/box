98c4b94a8178acac28e68ca30f5d50dc4975b3a0
vauxhall <zero.griffin@gmail.com>
2017-05-09 08:58:26 -0500

    store protocol

--- box/Model/Store/Base/MStoreProtocol.swift
+++ box/Model/Store/Base/MStoreProtocol.swift
@@ -1,10 +1,14 @@
 import UIKit
+import StoreKit
 
 protocol MStoreProtocol
 {
     var purchaseId:String { get }
     var title:String { get }
     var descr:String { get }
+    var skProduct:SKProduct? { get }
+    private(set) var price:String? { get }
+    private(set) var status:MStoreItemStatus? { get }
     
     func purchaseAction()
     func validatePurchase() -> Bool
