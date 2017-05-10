3293e85a01a8a50d21fa210c84e398ca30c885d9
vauxhall <zero.griffin@gmail.com>
2017-05-10 08:55:08 -0500

    controller settings

--- box/Controller/CSettings.swift
+++ box/Controller/CSettings.swift
@@ -4,10 +4,35 @@ class CSettings:CController
 {
     let model:MSettings
     private weak var viewSettings:VSettings!
+    private let urlMap:[String:String]?
+    private let kResourceName:String = "ResourceUrl"
+    private let kResourceExtension:String = "plist"
+    private let kReviewKey:String = "review"
+    private let kShareKey:String = "share"
     
     override init()
     {
         model = MSettings()
+        
+        guard
+            
+            let resourceUrl:URL = Bundle.main.url(
+                forResource:kResourceName,
+                withExtension:kResourceExtension),
+            let urlDictionary:NSDictionary = NSDictionary(
+                contentsOf:resourceUrl),
+            let urlMap:[String:String] = urlDictionary as? [String:String]
+            
+        else
+        {
+            self.urlMap = nil
+            
+            super.init()
+            
+            return
+        }
+        
+        self.urlMap = urlMap
         super.init()
     }
     
@@ -28,5 +53,46 @@ class CSettings:CController
     func back()
     {
         parentController.pop(horizontal:CParent.TransitionHorizontal.fromRight)
+    }
+    
+    func review()
+    {
+        guard
+            
+            let urlString:String = urlMap?[kReviewKey],
+            let url:URL = URL(string:urlString)
+            
+        else
+        {
+            return
+        }
+        
+        UIApplication.shared.openURL(url)
+    }
+    
+    func share()
+    {
+        guard
+            
+            let urlString:String = urlMap?[kShareKey],
+            let url:URL = URL(string:urlString)
+            
+        else
+        {
+            return
+        }
+        
+        let activity:UIActivityViewController = UIActivityViewController(
+            activityItems:[url],
+            applicationActivities:nil)
+        
+        if let popover:UIPopoverPresentationController = activity.popoverPresentationController
+        {
+            popover.sourceView = viewSettings
+            popover.sourceRect = CGRect.zero
+            popover.permittedArrowDirections = UIPopoverArrowDirection.up
+        }
+        
+        present(activity, animated:true)
     }
 }
