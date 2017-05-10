2f32aa066f431fc1c087f19edc7344cb2b6ee645
vauxhall <zero.griffin@gmail.com>
2017-05-10 07:49:40 -0500

    view settings cell share

---
+++ box/View/Settings/VSettingsCellReview.swift
@@ -0,0 +1,9 @@
+//
+//  VSettingsCellReview.swift
+//  box
+//
+//  Created by zero on 5/10/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ box/View/Settings/VSettingsCellShare.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class VSettingsCellShare:VSettingsCell
+{
+    
+}
