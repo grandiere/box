5b4c030dae55224c7482b320982f155636fcdc55
vauxhall <zero.griffin@gmail.com>
2017-05-10 17:46:55 -0500

    view help cell

---
+++ box/View/Help/VHelpCell.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+class VHelpCell:UICollectionViewCell
+{
+    private weak var imageView:UIImageView!
+    private weak var label:UILabel!
+    private weak var layoutLabelHeight:NSLayoutConstraint!
+    private let drawingOptions:NSStringDrawingOptions
+    private let labelMargin2:CGFloat
+    private let kLabelMargin:CGFloat = 20
+    private let kLabelMaxHeight:CGFloat = 900
+    private let kImageTop:CGFloat = 20
+    private let kImageHeight:CGFloat = 250
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        isUserInteractionEnabled = false
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        self.imageView = imageView
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.textAlignment = NSTextAlignment.center
+        label.numberOfLines = 0
+        self.label = label
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equalsHorizontal(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self,
+            constant:kImageTop)
+        NSLayoutConstraint.height(
+            view:imageView,
+            constant:kImageHeight)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config(model:MHelpProtocol)
+    {
+        imageView.image = model.image
+        label.attributedText = model.message
+        
+        let width:CGFloat = bounds.maxX
+        let usableWidth:CGFloat = width - labelMargin2
+        let usableSize:CGSize = CGSize(
+            width:usableWidth,
+            height:kLabelMaxHeight)
+        let textRect:CGRect = attributedText.boundingRect(
+            with:usableSize,
+            options:drawingOptions,
+            context:nil)
+        let textHeight:CGFloat = ceil(textRect.size.height)
+        layoutLabelHeight.constant = textHeight
+    }
+}
