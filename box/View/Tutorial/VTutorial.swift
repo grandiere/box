1fcd29ff559103d410598a4adc1029eac123e559
vauxhall <zero.griffin@gmail.com>
2017-05-11 09:29:51 -0500

    view tutorial layout

--- box/View/Tutorial/VTutorial.swift
+++ box/View/Tutorial/VTutorial.swift
@@ -3,11 +3,51 @@ import UIKit
 class VTutorial:VView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CTutorial!
+    private weak var collectionView:VCollection!
+    private let kBarHeight:CGFloat = 120
+    private let kCollectionBottom:CGFloat = 20
+    private let kCellHeight:CGFloat = 52
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CTutorial
+        
+        let viewBar:VTutorialBar = VTutorialBar(
+            controller:self.controller)
+        
+        let collectionView:VCollection = VCollection()
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(cell:VTutorialCell.self)
+        self.collectionView = collectionView
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.sectionInset = UIEdgeInsets(
+                top:kBarHeight,
+                left:0,
+                bottom:kCollectionBottom,
+                right:0)
+        }
+        
+        addSubview(collectionView)
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -15,7 +55,24 @@ class VTutorial:VView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
         return nil
     }
     
+    //MARK: private
+    
+    private func modelAtIndex(index:IndexPath) -> MTutorialProtocol
+    {
+        let item:MTutorialProtocol = controller.model.items[index.item]
+        
+        return item
+    }
+    
     //MARK: collectionView delegate
+    
+    func collectionView(_ collectionView:UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        let width:CGFloat = collectionView.bounds.maxX
+        let size:CGSize = CGSize(width:width, height:kCellHeight)
+        
+        return size
+    }
     
     func numberOfSections(in collectionView:UICollectionView) -> Int
     {
@@ -31,7 +88,13 @@ class VTutorial:VView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
     
     func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
     {
+        let item:MTutorialProtocol = modelAtIndex(index:indexPath)
         let cell:VTutorialCell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: <#T##String#>, for: <#T##IndexPath#>)
+            withReuseIdentifier:
+            VTutorialCell.reusableIdentifier,
+            for:indexPath) as! VTutorialCell
+        cell.config(model:item)
+        
+        return cell
     }
 }
