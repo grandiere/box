9dd2ae73accb18ece8b86bc1d6e6bbac18b0c3d6
vauxhall <zero.griffin@gmail.com>
2017-05-10 19:23:21 -0500

    view tutorial

--- box/View/Tutorial/VTutorial.swift
+++ box/View/Tutorial/VTutorial.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class VTutorial:VView
+class VTutorial:VView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CTutorial!
     
@@ -13,5 +13,25 @@ class VTutorial:VView
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: collectionView delegate
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.items.count
+        
+        return count
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell:VTutorialCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: <#T##String#>, for: <#T##IndexPath#>)
     }
 }
