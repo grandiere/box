1063e7bc065eb63eba819682e28f8d57056acff2
vauxhall <zero.griffin@gmail.com>
2017-05-11 09:13:36 -0500

    view tutorial cell

--- box/View/Tutorial/VTutorialCell.swift
+++ box/View/Tutorial/VTutorialCell.swift
@@ -2,5 +2,26 @@ import UIKit
 
 class VTutorialCell:UICollectionViewCell
 {
+    private weak var label:UILabel!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let label:UILabel = UILabel()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config(model:MTutorialProtocol)
+    {
+        
+    }
 }
