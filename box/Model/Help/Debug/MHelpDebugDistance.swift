3228e6612eebd17832fb2275f05cfe3192a1538f
vauxhall <zero.griffin@gmail.com>
2017-05-11 17:21:16 -0500

    debug distance item

---
+++ box/Model/Help/Debug/MHelpDebugDistance.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class MHelpDebugDistance:MHelpProtocol
+{
+    private let attributedString:NSAttributedString
+    
+    init()
+    {
+        let attributesTitle:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.bold(size:20),
+            NSForegroundColorAttributeName:UIColor.white]
+        let attributesDescription:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.regular(size:18),
+            NSForegroundColorAttributeName:UIColor(white:1, alpha:0.8)]
+        
+        let stringTitle:NSAttributedString = NSAttributedString(
+            string:NSLocalizedString("MHelpDebugDistance_title", comment:""),
+            attributes:attributesTitle)
+        let stringDescription:NSAttributedString = NSAttributedString(
+            string:NSLocalizedString("MHelpDebugDistance_description", comment:""),
+            attributes:attributesDescription)
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        mutableString.append(stringTitle)
+        mutableString.append(stringDescription)
+        
+        attributedString = mutableString
+    }
+    
+    var message:NSAttributedString
+    {
+        get
+        {
+            return attributedString
+        }
+    }
+    
+    var image:UIImage
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetHelpDebugDistance")
+        }
+    }
+}
