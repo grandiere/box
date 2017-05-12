511fb930d65ef53616dc108eda84d3cc875943d2
vauxhall <zero.griffin@gmail.com>
2017-05-12 15:22:38 -0500

    model help aids

---
+++ box/Model/Help/Aids/MHelpAids.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class MHelpAids:MHelp
+{
+    init()
+    {
+        let itemIntro:MHelpSkillsIntro = MHelpSkillsIntro()
+        let itemAids:MHelpSkillsAids = MHelpSkillsAids()
+        let itemPurchase:MHelpSkillsPurchase = MHelpSkillsPurchase()
+        
+        let items:[MHelpProtocol] = [
+            itemIntro,
+            itemAids,
+            itemPurchase]
+        
+        super.init(items:items)
+    }
+}
