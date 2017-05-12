e1d0e34871049d2df2f25e5657b8805f67f6cd2a
vauxhall <zero.griffin@gmail.com>
2017-05-11 19:22:45 -0500

    anti virus intro

--- box/Model/Help/Antivirus/MHelpAntivirus.swift
+++ box/Model/Help/Antivirus/MHelpAntivirus.swift
@@ -4,14 +4,10 @@ class MHelpAntivirus:MHelp
 {
     init()
     {
-        let itemIntro:MHelpDebugIntro = MHelpDebugIntro()
-        let itemStats:MHelpDebugStats = MHelpDebugStats()
-        let itemDistance:MHelpDebugDistance = MHelpDebugDistance()
+        let itemIntro:MHelpAntivirusIntro = MHelpAntivirusIntro()
         
         let items:[MHelpProtocol] = [
-            itemIntro,
-            itemStats,
-            itemDistance]
+            itemIntro]
         
         super.init(items:items)
     }
