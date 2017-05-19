8d592ea34ee3a13de2e354cc8d4518c6cda2f3f2
vauxhall <zero.griffin@gmail.com>
2017-05-19 14:32:19 -0500

    firebase db harvest item

---
+++ box/Firebase/Database/Model/FDbHarvestItem.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+class FDbHarvestItem:FDbProtocol
+{
+    static let score:String = "score"
+    static let kills:String = "kills"
+    
+    let score:Int
+    let kills:Int
+    private let kDefault:Int = 0
+    
+    init()
+    {
+        score = kDefault
+        kills = kDefault
+    }
+    
+    //MARK: node protocol
+    
+    required init?(snapshot:Any)
+    {
+        let snapshotDict:[String:Any]? = snapshot as? [String:Any]
+        
+        if let score:Int = snapshotDict?[
+            FDbHarvestItem.score] as? Int
+        {
+            self.score = score
+        }
+        else
+        {
+            score = kDefault
+        }
+        
+        if let kills:Int = snapshotDict?[
+            FDbHarvestItem.kills] as? Int
+        {
+            self.kills = kills
+        }
+        else
+        {
+            kills = kDefault
+        }
+    }
+    
+    func json() -> Any?
+    {
+        var json:[String:Any] = [
+            FDbHarvestItem.score:score,
+            FDbHarvestItem.kills:kills]
+        
+        return json
+    }
+}
