e21c0bf747d7e1ef01600da75eabe66b85e15b1c
vauxhall <zero.griffin@gmail.com>
2017-05-20 02:00:44 -0500

    view grid harvest bar

--- box/View/GridHarvest/VGridHarvest.swift
+++ box/View/GridHarvest/VGridHarvest.swift
@@ -3,11 +3,20 @@ import UIKit
 class VGridHarvest:VView
 {
     private weak var controller:CGridHarvest!
+    private weak var viewBar:VGridHarvestBar!
+    private let kBarHeight:CGFloat = 150
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CGridHarvest
+        
+        let viewBar:VGridHarvestBar = VGridHarvestBar(controller:self.controller)
+        self.viewBar = viewBar
+        
+        addSubview(viewBar)
+        
+        
     }
     
     required init?(coder:NSCoder)
---
+++ box/View/GridHarvest/VGridHarvestBar.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class VGridHarvestBar:UIView
+{
+    private weak var controller:CGridHarvest!
+    
+    init(controller:CGridHarvest)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
