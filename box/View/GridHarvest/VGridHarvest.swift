1ed1bbab601318ab78a9455a6a8933d39ec47b66
vauxhall <zero.griffin@gmail.com>
2017-05-20 01:53:02 -0500

    view.controller harvest

--- box/View/GridHarvest/VGridHarvest.swift
+++ box/View/GridHarvest/VGridHarvest.swift
@@ -2,5 +2,16 @@ import UIKit
 
 class VGridHarvest:VView
 {
+    private weak var controller:CGridHarvest!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CGridHarvest
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
