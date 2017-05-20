44bac0ec7d29ea507a82a1f7ccf2d5c7b5bf6d42
vauxhall <zero.griffin@gmail.com>
2017-05-20 01:53:59 -0500

    controller view harvest

--- box/Controller/CGridHarvest.swift
+++ box/Controller/CGridHarvest.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CGridHarvest:CController
 {
+    private weak var viewHarvest:VGridHarvest!
     
+    override func loadView()
+    {
+        let viewHarvest:VGridHarvest = VGridHarvest(controller:self)
+        self.viewHarvest = viewHarvest
+        view = viewHarvest
+    }
 }
