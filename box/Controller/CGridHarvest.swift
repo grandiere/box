8fe07288994f6618f0d6b966ab6b844082671f1d
vauxhall <zero.griffin@gmail.com>
2017-05-22 10:52:21 -0500

    model grid harvest

--- box/Controller/CGridHarvest.swift
+++ box/Controller/CGridHarvest.swift
@@ -11,10 +11,52 @@ class CGridHarvest:CController
         view = viewHarvest
     }
     
+    //MARK: private
+    
+    private func asyncLoadHarvest()
+    {
+        guard
+            
+            let userId:String = MSession.sharedInstance.settings?.firebaseId
+        
+        else
+        {
+            return
+        }
+        
+        let path:String = "\(FDb.harvest)/\(userId)/"
+        
+        FMain.sharedInstance.db.listenOnce(
+            path:path,
+            nodeType:FDbHarvestItem.self)
+        { [weak self] (data:FDbProtocol?) in
+            
+            guard
+            
+                let harvestItem:FDbHarvestItem = data as? FDbHarvestItem
+            
+            else
+            {
+                return
+            }
+            
+            
+        }
+    }
+    
     //MARK: public
     
     func back()
     {
         parentController.pop(horizontal:CParent.TransitionHorizontal.fromRight)
+    }
+    
+    func loadHarvest()
+    {
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.asyncLoadHarvest()
+        }
     }
 }
