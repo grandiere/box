1396c9ceca81c2f30fb8819c5c46ba86daf39637
vauxhall <zero.griffin@gmail.com>
2017-05-23 18:52:56 -0500

    finder render protocol

--- box/Model/GridVisor/Render/MGridVisorRenderFinder.swift
+++ box/Model/GridVisor/Render/MGridVisorRenderFinder.swift
@@ -1,6 +1,25 @@
 import Foundation
+import MetalKit
 
-class MGridVisorRenderFinder
+class MGridVisorRenderFinder:MetalRenderableProtocol
 {
+    //MARK: renderable Protocol
     
+    func render(renderEncoder:MTLRenderCommandEncoder)
+    {
+        guard
+            
+            let texture:MTLTexture = self.texture
+            
+        else
+        {
+            return
+        }
+        
+        renderEncoder.render(
+            vertex:spatialSquare.vertexBuffer,
+            position:positionBuffer,
+            rotation:rotationBuffer,
+            texture:texture)
+    }
 }
