383085c0486b45f0725591ff278a6a32ad321f5e
vauxhall <zero.griffin@gmail.com>
2017-05-23 16:16:14 -0500

    removed back white

--- box/View/GridVisor/VGridVisorBarBack.swift
+++ box/View/GridVisor/VGridVisorBarBack.swift
@@ -2,5 +2,19 @@ import UIKit
 
 class VGridVisorBarBack:UIButton
 {
+    private weak var controller:CGridVisor!
     
+    init(controller:CGridVisor)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
