de96a0907e7d5f2eba5392e55405fb766886e164
vauxhall <zero.griffin@gmail.com>
2017-05-24 18:57:02 -0500

    cell actions cell

--- box/View/GridVisorDetail/VGridVisorDetailCellActions.swift
+++ box/View/GridVisorDetail/VGridVisorDetailCellActions.swift
@@ -1,6 +1,42 @@
 import UIKit
 
-class VGridVisorDetailCellActions:VGridVisorDetailCell
+class VGridVisorDetailCellActions:VGridVisorDetailCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewFlowLayout
 {
+    private weak var collectionView:VCollection!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let collectionView:VCollection = VCollection()
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: collectionView delegate
+    
+    func numberOfSections(in collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell:vcolle
+    }
 }
