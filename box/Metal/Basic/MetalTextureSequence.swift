cf8364307793b5b7239b6564b849adb7d284ef25
vauxhall <zero.griffin@gmail.com>
2017-05-24 13:57:47 -0500

    metal texture sequence

---
+++ box/Metal/Basic/MetalTextureSequence.swift
@@ -0,0 +1,74 @@
+import UIKit
+import MetalKit
+
+class MetalTextureSequence
+{
+    private(set) var currentFrame:Int
+    private(set) var frameTick:Int
+    private let frames:[MTLTexture]
+    private let ticksPerFrame:Int
+    private let totalFrames:Int
+    
+    init(
+        ticksPerFrame:Int,
+        images:[UIImage],
+        textureLoader:MTKTextureLoader)
+    {
+        self.ticksPerFrame = ticksPerFrame
+        currentFrame = 0
+        frameTick = 0
+        
+        var frames:[MTLTexture] = []
+        
+        for image:UIImage in images
+        {
+            guard
+            
+                let texture:MTLTexture = textureLoader.loadImage(image:image)
+            
+            else
+            {
+                continue
+            }
+            
+            frames.append(texture)
+        }
+        
+        self.frames = frames
+        totalFrames = frames.count
+    }
+    
+    //MARK: final
+    
+    final func current() -> MTLTexture?
+    {
+        if totalFrames < 1
+        {
+            return nil
+        }
+        
+        frameTick += 1
+        
+        if frameTick >= ticksPerFrame
+        {
+            frameTick = 0
+            
+            currentFrame += 1
+            
+            if currentFrame >= totalFrames
+            {
+                currentFrame = 0
+            }
+        }
+        
+        let texture:MTLTexture = frames[currentFrame]
+        
+        return texture
+    }
+    
+    final func restart()
+    {
+        currentFrame = 0
+        frameTick = 0
+    }
+}
