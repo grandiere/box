a6f471cc566baae1cf4afb64a8906af0917ef3c6
vauxhall <zero.griffin@gmail.com>
2017-05-24 14:01:00 -0500

    m grid visor render sequence

---
+++ box/Model/GridVisor/Render/MGridVisorRenderFinderSequence.swift
@@ -0,0 +1,26 @@
+import UIKit
+import MetalKit
+
+class MGridVisorRenderFinderSequence:MetalTextureSequence
+{
+    private let kTicksPerFrame:Int = 30
+    
+    init(textureLoader:MTKTextureLoader)
+    {
+        let images:[UIImage] = [
+            #imageLiteral(resourceName: "assetTextureTarget01"),
+            #imageLiteral(resourceName: "assetTextureTarget02"),
+            #imageLiteral(resourceName: "assetTextureTarget03"),
+            #imageLiteral(resourceName: "assetTextureTarget04"),
+            #imageLiteral(resourceName: "assetTextureTarget05"),
+            #imageLiteral(resourceName: "assetTextureTarget06"),
+            #imageLiteral(resourceName: "assetTextureTarget07"),
+            #imageLiteral(resourceName: "assetTextureTarget08"),
+            #imageLiteral(resourceName: "assetTextureTarget09")]
+        
+        super.init(
+            ticksPerFrame:kTicksPerFrame,
+            images:images,
+            textureLoader:textureLoader)
+    }
+}
