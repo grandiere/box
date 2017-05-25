0898873c0565dfd55f778b5752a3d1e8a89d9420
vauxhall <zero.griffin@gmail.com>
2017-05-25 09:47:20 -0500

    view grid visor focus

---
+++ box/View/GridVisorFocus/VGridVisorFocus.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class VGridVisorFocus:VView
+{
+    private weak var controller:CGridVisorFocus!
+    
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        self.controller = controller as? CGridVisorFocus
+    }
+}
