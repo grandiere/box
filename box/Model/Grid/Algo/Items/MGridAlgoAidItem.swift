eceba9a07256c4b900fe010411e94a3a376073f1
vauxhall <zero.griffin@gmail.com>
2017-05-25 13:54:38 -0500

    refactor grid algo item

--- box/Model/Grid/Algo/Items/MGridAlgoAidItem.swift
+++ box/Model/Grid/Algo/Items/MGridAlgoAidItem.swift
@@ -15,65 +15,94 @@ class MGridAlgoAidItem:MGridAlgoItem
             created:firebaseAid.created)
     }
     
-    /*
-    override func firebasePath() -> String
+    //MARK: algo protocol
+    
+    override var firebasePath:String
     {
-        let path:String = "\(FDb.algoAid)/\(firebaseId)"
+        get
+        {
+            let path:String = "\(FDb.algoAid)/\(firebaseId)"
+            
+            return path
+        }
+    }
+    
+    override var icon:UIImage?
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetTextureAidDetail")
+        }
+    }
+    
+    override func detail() -> [MGridVisorDetailProtocol]
+    {
+        let items:[MGridVisorDetailProtocol] = []
         
-        return path
+        return items
     }
     
-    override func imageStandby() -> UIImage?
-    {
-        return #imageLiteral(resourceName: "assetTextureAidStandBy")
-    }
-    
-    override func imageTargeted() -> UIImage?
-    {
-        return #imageLiteral(resourceName: "assetTextureAidTargeted")
-    }
-    
-    override func imageDetail() -> UIImage?
-    {
-        return #imageLiteral(resourceName: "assetTextureAidDetail")
-    }
-    
-    override func detail() -> MGridVisorDetail
-    {
-        let model:MGridVisorDetail = MGridVisorDetail.detailAid(model:self)
-        
-        return model
-    }
-    
-    override func annotationImageOn() -> UIImage?
-    {
-        return #imageLiteral(resourceName: "assetTextureAidAnnotationOn")
-    }
-    
-    override func annotationImageOff() -> UIImage?
-    {
-        return #imageLiteral(resourceName: "assetTextureAidAnnotationOff")
-    }
-    
-    override func titleAnnotation() -> String?
-    {
-        let title:String = NSLocalizedString("MGridAlgoItemAid_titleAnnotation", comment:"")
-        
-        return title
-    }
-    
-    override func textureColour() -> UIColor
-    {
-        return UIColor.gridBlue
-    }
+    //MARK: menu protocol
     
     override var showDownload:Bool
-        {
+    {
         get
         {
             return kShowDownload
         }
     }
- 
- */
+    
+    //MARK: render protocol
+    
+    override var textureStandby:UIImage?
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetTextureAidStandBy")
+        }
+    }
+    
+    override var textureTargeted:UIImage?
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetTextureAidTargeted")
+        }
+    }
+    
+    override var overlayColour:UIColor
+    {
+        get
+        {
+            return UIColor.gridBlue
+        }
+    }
+    
+    //MARK: map protocol
+    
+    override var annotationImageOn:UIImage?
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetTextureAidAnnotationOn")
+        }
+    }
+    
+    override var annotationImageOff:UIImage?
+    {
+        get
+        {
+            return #imageLiteral(resourceName: "assetTextureAidAnnotationOff")
+        }
+    }
+    
+    override var titleAnnotation:String?
+    {
+        get
+        {
+            let title:String = NSLocalizedString("MGridAlgoItemAid_titleAnnotation", comment:"")
+            
+            return title
+        }
+    }
 }
