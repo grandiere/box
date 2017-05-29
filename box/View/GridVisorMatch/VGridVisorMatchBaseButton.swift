b92a6ea2ff6dc43155b1581d31dda8e0021a3e15
vauxhall <zero.griffin@gmail.com>
2017-05-28 21:28:42 -0500

    match base button init

--- box/View/GridVisorMatch/VGridVisorMatchBaseButton.swift
+++ box/View/GridVisorMatch/VGridVisorMatchBaseButton.swift
@@ -1,9 +1,26 @@
-//
-//  VGridVisorMatchBaseButton.swift
-//  box
-//
-//  Created by zero on 5/28/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VGridVisorMatchBaseButton:UIButton
+{
+    init(image:UIImage, tintColor:UIColor)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        setImage(
+            image.withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        setImage(
+            image.withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        imageView!.tintColor = tintColor
+        imageView!.clipsToBounds = true
+        imageView!.contentMode = UIViewContentMode.center
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
