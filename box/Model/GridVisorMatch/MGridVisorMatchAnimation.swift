7afcb6e047e738df0aafe3c10afb3a8416c53f97
vauxhall <zero.griffin@gmail.com>
2017-05-29 15:40:57 -0500

    factory match animation

--- box/Model/GridVisorMatch/MGridVisorMatchAnimation.swift
+++ box/Model/GridVisorMatch/MGridVisorMatchAnimation.swift
@@ -1,11 +1,53 @@
-import Foundation
+import UIKit
 
 class MGridVisorMatchAnimation
 {
     var items:[MGridVisorMatchAnimationItem]
+    private let minX:CGFloat
+    private let minY:CGFloat
+    private var maxX:CGFloat
+    private var maxY:CGFloat
+    private let kMargin:CGFloat = 50
+    private let kPossibleItems:[String] = ["0","1"]
+    private let kCreateRatio:UInt32 = 10
+    private let kInitialItems:Int = 20
     
     init()
     {
         items = []
+        minX = -kMargin
+        minY = -kMargin
+        maxX = 0
+        maxY = 0
+    }
+    
+    //MARK: private
+    
+    private func createItem()
+    {
+        
+    }
+    
+    //MARK: public
+    
+    func initialElements(width:CGFloat, height:CGFloat)
+    {
+        maxX = width + kMargin
+        maxY = height + kMargin
+        
+        for _:Int in 0 ..< kInitialItems
+        {
+            createItem()
+        }
+    }
+    
+    func tick()
+    {
+        let shouldCreate:UInt32 = arc4random_uniform(kCreateRatio)
+        
+        if shouldCreate == 0
+        {
+            createItem()
+        }
     }
 }
--- box/Model/GridVisorMatch/MGridVisorMatchAnimationItem.swift
+++ box/Model/GridVisorMatch/MGridVisorMatchAnimationItem.swift
@@ -2,15 +2,38 @@ import UIKit
 
 class MGridVisorMatchAnimationItem
 {
+    enum Direction:CGFloat
+    {
+        case left = -1
+        case right = 1
+    }
+    
+    let direction:Direction
     let item:String
     let alpha:CGFloat
     var posX:CGFloat
     var posY:CGFloat
     
-    init(item:String, alpha:CGFloat)
+    class func randomDirection() -> Direction
+    {
+        let random:UInt32 = arc4random_uniform(2)
+        
+        if random == 0
+        {
+            return Direction.left
+        }
+        
+        return Direction.right
+    }
+    
+    init(
+        item:String,
+        alpha:CGFloat,
+        direction:Direction)
     {
         self.item = item
         self.alpha = alpha
+        self.direction = direction
         posX = 0
         posY = 0
     }
