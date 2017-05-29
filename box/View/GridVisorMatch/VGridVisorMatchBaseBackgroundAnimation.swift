9a7933c5da1a8bfab01b87056a28f58520cdacb7
vauxhall <zero.griffin@gmail.com>
2017-05-29 17:10:43 -0500

    init background animation

--- box/View/GridVisorMatch/VGridVisorMatchBaseBackgroundAnimation.swift
+++ box/View/GridVisorMatch/VGridVisorMatchBaseBackgroundAnimation.swift
@@ -2,12 +2,47 @@ import UIKit
 
 class VGridVisorMatchBaseBackgroundAnimation:UIView
 {
+    private let model:MGridVisorMatchAnimation
+    private let kWaitInit:TimeInterval = 0.5
+    
     init()
     {
+        model = MGridVisorMatchAnimation()
+        
         super.init(frame:CGRect.zero)
         clipsToBounds = true
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
+        
+        DispatchQueue.main.asyncAfter(
+            deadline:DispatchTime.now() + kWaitInit)
+        { [weak self] in
+            
+            guard
+                
+                let width:CGFloat = self?.bounds.maxX,
+                let height:CGFloat = self?.bounds.maxY
+            
+            else
+            {
+                return
+            }
+            
+            DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+            { [weak self] in
+                
+                guard
+                
+                    let model:MGridVisorMatchAnimation = self?.model
+                
+                else
+                {
+                    return
+                }
+                
+                model.initialElements(width:width, height:height)
+            }
+        }
     }
     
     required init?(coder:NSCoder)
