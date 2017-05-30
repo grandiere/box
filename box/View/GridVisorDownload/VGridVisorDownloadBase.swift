8e1be8caab710258797ee36bc0675aa5ccf7d9ab
vauxhall <zero.griffin@gmail.com>
2017-05-30 12:38:52 -0500

    download base layout

--- box/View/GridVisorDownload/VGridVisorDownloadBase.swift
+++ box/View/GridVisorDownload/VGridVisorDownloadBase.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class VGridVisorDownloadBase:UIView
 {
+    private weak var imageView:UIImageView!
+    private let kAnimationDuration:TimeInterval = 1
+    
     init()
     {
         super.init(frame:CGRect.zero)
@@ -10,17 +13,43 @@ class VGridVisorDownloadBase:UIView
         translatesAutoresizingMaskIntoConstraints = false
         isUserInteractionEnabled = false
         
+        let images:[UIImage] = [
+            #imageLiteral(resourceName: "assetDownload0"),
+            #imageLiteral(resourceName: "assetDownload1"),
+            #imageLiteral(resourceName: "assetDownload2"),
+            #imageLiteral(resourceName: "assetDownload3"),
+            #imageLiteral(resourceName: "assetDownload4")]
+        
         let blur:VBlur = VBlur.light()
         
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.isUserInteractionEnabled = false
+        imageView.animationImages = images
+        imageView.animationDuration = kAnimationDuration
+        imageView.startAnimating()
+        self.imageView = imageView
+        
         addSubview(blur)
+        addSubview(imageView)
         
         NSLayoutConstraint.equals(
             view:blur,
+            toView:self)
+        NSLayoutConstraint.equals(
+            view:imageView,
             toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    deinit
+    {
+        imageView.stopAnimating()
     }
 }
