8527c8dce03444fbbc2c2b891c09e8868521bf9d
vauxhall <zero.griffin@gmail.com>
2017-05-30 10:41:15 -0500

    assets download

--- box/View/GridVisorDownload/VGridVisorDownload.swift
+++ box/View/GridVisorDownload/VGridVisorDownload.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class VGridVisorDownload:VView
 {
+    private weak var controller:CGridVisorDownload!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        self.controller = controller as? CGridVisorDownload
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
