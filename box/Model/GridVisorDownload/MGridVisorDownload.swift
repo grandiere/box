541e84169f48fa5bfb0808766ffa7f33012aa1cf
vauxhall <zero.griffin@gmail.com>
2017-05-30 17:23:54 -0500

    cleaned up mgrid visor download

--- box/Model/GridVisorDownload/MGridVisorDownload.swift
+++ box/Model/GridVisorDownload/MGridVisorDownload.swift
@@ -2,33 +2,12 @@ import Foundation
 
 class MGridVisorDownload
 {
-    private weak var controller:CGridVisorDownload?
-    private weak var model:MGridAlgoAidItem!
-    private let kDifficultyStats:UInt32 = 80
+    private static let kDifficultyStats:UInt32 = 80
     
     class func strategyDownload(item:MGridAlgoAidItem) -> MGridVisorDownloadProtocol?
     {
-        
-    }
-    
-    init(controller:CGridVisorDownload, model:MGridAlgoAidItem)
-    {
-        self.controller = controller
-        self.model = model
-        
-        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
-        { [weak self] in
-            
-            self?.strategyDownload()
-        }
-    }
-    
-    //MARK: private
-    
-    private func strategyDownload()
-    {
         let random:UInt32 = arc4random_uniform(kDifficultyStats)
-        let strategy:MGridVisorDownloadProtocol
+        let strategy:MGridVisorDownloadProtocol?
         
         if random == 0
         {
@@ -36,33 +15,9 @@ class MGridVisorDownload
         }
         else
         {
-            guard
-            
-                let strategyEnergy:MGridVisorDownloadEnergy = MGridVisorDownloadEnergy.factory()
-            
-            else
-            {
-                return
-            }
-            
-            strategy = strategyEnergy
+            strategy = MGridVisorDownloadEnergy.factory()
         }
         
-        guard
-            
-            let controller:CGridVisorDownload = self.controller
-            
-        else
-        {
-            return
-        }
-        
-        strategy.apply(controller:controller)
-        
-        DispatchQueue.main.async
-        { [weak self] in
-            
-            self?.controller?.viewTake.aidDefined(model:strategy)
-        }
+        return strategy
     }
 }
