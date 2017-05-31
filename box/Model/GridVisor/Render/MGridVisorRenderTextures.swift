346303045bef0e13f9145aacddf90d2007c87be4
vauxhall <zero.griffin@gmail.com>
2017-05-30 20:21:42 -0500

    init textures

--- box/Model/GridVisor/Render/MGridVisorRenderTextures.swift
+++ box/Model/GridVisor/Render/MGridVisorRenderTextures.swift
@@ -1,6 +1,11 @@
-import Foundation
+import UIKit
+import MetalKit
 
 class MGridVisorRenderTextures
 {
-    
+    init(
+        device:MTLDevice,
+        textureLoader:MTKTextureLoader)
+    {
+    }
 }
