71c72484f74decf25541e3a6f8673a63968fa93c
vauxhall <zero.griffin@gmail.com>
2017-05-31 11:23:45 -0500

    init textures

--- box/Model/GridVisor/Render/Textures/MGridVisorRenderTextures.swift
+++ box/Model/GridVisor/Render/Textures/MGridVisorRenderTextures.swift
@@ -3,10 +3,10 @@ import MetalKit
 
 class MGridVisorRenderTextures
 {
-    let standbyAid:MTLTexture
-    let standbyBug:MTLTexture
-    let standbyVirusFoe:MTLTexture
-    let standbyVirusFriendly:MTLTexture
+    let standbyAid:MTLTexture?
+    let standbyBug:MTLTexture?
+    let standbyVirusFoe:MTLTexture?
+    let standbyVirusFriendly:MTLTexture?
     let targetedAid:MGridVisorRenderTexturesAid
     let targetedBug:MGridVisorRenderTexturesBug
     let targetedVirusFoe:MGridVisorRenderTexturesVirusFoe
@@ -14,6 +14,22 @@ class MGridVisorRenderTextures
     
     init(textureLoader:MTKTextureLoader)
     {
+        standbyAid = textureLoader.loadImage(
+            image:#imageLiteral(resourceName: "assetTextureAidStandBy"))
+        standbyBug = textureLoader.loadImage(
+            image:#imageLiteral(resourceName: "assetTextureBugStandBy"))
+        standbyVirusFoe = textureLoader.loadImage(
+            image:#imageLiteral(resourceName: "assetTextureVirusFoeStandBy"))
+        standbyVirusFriendly = textureLoader.loadImage(
+            image:#imageLiteral(resourceName: "assetTextureVirusFriendlyStandBy"))
         
+        targetedAid = MGridVisorRenderTexturesAid(
+            textureLoader:textureLoader)
+        targetedBug = MGridVisorRenderTexturesBug(
+            textureLoader:textureLoader)
+        targetedVirusFoe = MGridVisorRenderTexturesVirusFoe(
+            textureLoader:textureLoader)
+        targetedVirusFriendly = MGridVisorRenderTexturesVirusFriendly(
+            textureLoader:textureLoader)
     }
 }
