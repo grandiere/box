fdb0e359bff91e4e9800fde6b7342ea53eae17be
vauxhall <zero.griffin@gmail.com>
2017-05-31 17:35:06 -0500

    metal texture protocol

---
+++ box/Metal/Basic/MetalTextureProtocol.swift
@@ -0,0 +1,7 @@
+import Foundation
+import MetalKit
+
+protocol MetalTextureProtocol
+{
+    func current() -> MTLTexture?
+}
--- box/Model/GridVisor/Render/Textures/MGridVisorRenderTexturesBug.swift
+++ box/Model/GridVisor/Render/Textures/MGridVisorRenderTexturesBug.swift
@@ -3,12 +3,15 @@ import MetalKit
 
 class MGridVisorRenderTexturesBug:MetalTextureSequence
 {
-    private let kTicksPerFrame:Int = 20
+    private let kTicksPerFrame:Int = 50
     
     init(textureLoader:MTKTextureLoader)
     {
         let images:[UIImage] = [
-            #imageLiteral(resourceName: "assetTextureBugTargeted")]
+            #imageLiteral(resourceName: "assetTextureBugTargeted0"),
+            #imageLiteral(resourceName: "assetTextureBugTargeted1"),
+            #imageLiteral(resourceName: "assetTextureBugTargeted2"),
+            #imageLiteral(resourceName: "assetTextureBugTargeted3")]
         
         super.init(
             ticksPerFrame:kTicksPerFrame,
