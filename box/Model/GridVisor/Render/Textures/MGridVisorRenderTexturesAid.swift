551134a08d3795f354cfbe0d18ae580e97fe7d11
vauxhall <zero.griffin@gmail.com>
2017-05-31 09:44:55 -0500

    grid visor render textures aid

---
+++ box/Model/GridVisor/Render/Textures/MGridVisorRenderTexturesAid.swift
@@ -0,0 +1,24 @@
+import UIKit
+import MetalKit
+
+class MGridVisorRenderTexturesAid:MetalTextureSequence
+{
+    private let kTicksPerFrame:Int = 10
+    
+    init(textureLoader:MTKTextureLoader)
+    {
+        let images:[UIImage] = [
+            #imageLiteral(resourceName: "assetTextureAidTargeted0"),
+            #imageLiteral(resourceName: "assetTextureAidTargeted1"),
+            #imageLiteral(resourceName: "assetTextureAidTargeted2"),
+            #imageLiteral(resourceName: "assetTextureAidTargeted3"),
+            #imageLiteral(resourceName: "assetTextureAidTargeted4"),
+            #imageLiteral(resourceName: "assetTextureAidTargeted5"),
+            #imageLiteral(resourceName: "assetTextureAidTargeted6")]
+        
+        super.init(
+            ticksPerFrame:kTicksPerFrame,
+            images:images,
+            textureLoader:textureLoader)
+    }
+}
